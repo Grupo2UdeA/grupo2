e3508ed15a9ab4ebf28503740fc175b247b0cfee
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-07-12 16:40:01 -0500

    Se arregló el error de que la aplicación se rompía al no colocar nada dentro de los inputs, también se hicieron cambios en las interfaces para que lucieran similares entre ellas. Todo esto con las figuras: Rectangulo, Cuadrado y Circulo

--- app/src/main/java/com/arlequins/grupo2/ui/square/SquareActivity.kt
+++ app/src/main/java/com/arlequins/grupo2/ui/square/SquareActivity.kt
@@ -1,9 +1,12 @@
 package com.arlequins.grupo2.ui.square
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.arlequins.grupo2.R
 import com.arlequins.grupo2.databinding.ActivitySquareBinding
 
 
@@ -22,18 +25,37 @@ class SquareActivity : AppCompatActivity() {
         squareViewModel = ViewModelProvider(this)[SquareViewModel::class.java]
         setContentView(view)
 
-        squareViewModel.resultP.observe(this){
+        squareViewModel.resultP.observe(this) {
             squareBinding.answerPerimeterTextView.text = it
         }
 
-        squareViewModel.resultA.observe(this){
+        squareViewModel.resultA.observe(this) {
             squareBinding.answerAreaTextView.text = it
         }
+        squareBinding.answerAreaTextView.visibility = TextView.VISIBLE
+        squareBinding.answerPerimeterTextView.visibility = TextView.VISIBLE
 
-        with(squareBinding){
-            calculateButton.setOnClickListener{
-                squareViewModel.obtainSide(sideSquareInputText.text.toString())
-                squareViewModel.calculateAll()
+        with(squareBinding) {
+            calculateButton.setOnClickListener {
+                if (squareBinding.sideSquareInputText.text.toString()
+                        .isNotEmpty()
+                ) {
+                    squareViewModel.obtainSide(sideSquareInputText.text.toString())
+                    squareViewModel.calculateAll()
+                    squareBinding.answerAreaTextView.visibility = TextView.VISIBLE
+                    squareBinding.answerPerimeterTextView.visibility = TextView.VISIBLE
+                } else {
+                    Toast.makeText(
+                        this@SquareActivity,
+                        getString(R.string.emptyInput),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    squareBinding.answerAreaTextView.visibility = TextView.INVISIBLE
+                    squareBinding.answerPerimeterTextView.visibility = TextView.INVISIBLE
+
+
+                }
+
             }
         }
     }
